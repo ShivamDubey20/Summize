f565e0e0960ddfcc622654937f5b2413aa8edddd
shivamkd20@gmail.com <shivam.dubey2020@vitbhopal.ac.in>
2023-10-04 01:11:22 +0530

    Summize

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Hero from './Components/Hero'
+import Demo from './Components/Demo'
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <main>
+      <div className='main'>
+        <div className='gradient' />
+      </div>
+
+      <div className='app'>
+        <Hero />
+        <Demo />
+      </div>
+    </main>
+  );
+};
+
+export default App;
